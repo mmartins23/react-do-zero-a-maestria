9133bf4fb2ffc70089a6b2cf925353078719843d
Marlon Martins <marlon-martins@outlook.com>
2024-04-08 17:29:16 +0200

    useEffect with Values in the Dependency Array

--- 9- React Hooks/react-hooks/src/components/HookUseEffect.js
+++ 9- React Hooks/react-hooks/src/components/HookUseEffect.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react"
 const HookUseEffect = () => {
 
     const [number, setNumber] = useState(1);
+    const [luckyNumber, setLuckyNumber] = useState(1);
+
 
     //1- useEffect without dependencies
     useEffect(() => {
@@ -18,13 +20,20 @@ const HookUseEffect = () => {
         console.log("This useEffect will be run only once!")
     },[])
 
+     //3- useEffect with values in the array dependencies
+     useEffect(() => {
+        console.log("This useEffect will only run when luckyNumber is modified");
+    },[luckyNumber])
+
     return (
         <div>
             <h1>useEffect</h1>
             <p>Number: {number}</p>
             <button onClick={updateNumber}>Increase Number</button>
+            <p>Lucky Number: {luckyNumber}</p>
+            <button onClick={() => setLuckyNumber(luckyNumber+1)}>Increase number</button>
         </div>
     )
 }
 
-export default HookUseEffect
+export default HookUseEffect;
