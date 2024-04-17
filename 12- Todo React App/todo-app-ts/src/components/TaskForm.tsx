c01cbf97b4e18abc93993f85bc41d208adcc681c
Marlon Martins <marlon-martins@outlook.com>
2024-04-17 16:12:13 +0200

    CSS Form

--- 12- Todo React App/todo-app-ts/src/components/TaskForm.tsx
+++ 12- Todo React App/todo-app-ts/src/components/TaskForm.tsx
@@ -1,16 +1,19 @@
+// CSS
+import styles from "./TaskForm.module.css";
+
 type Props = {
   btnText: string;
 }
 
-const TaskForm = ({btnText}: Props) => {
+const TaskForm = ({ btnText }: Props) => {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.input_container}>
         <label htmlFor="title">Título:</label>
         <input type="text" name="title" placeholder="Titulo da tarefa" />
       </div>
 
-      <div>
+      <div className={styles.input_container}>
         <label htmlFor="difficulty">Dificuldade:</label>
         <input type="text" name="difficulty" placeholder="Dificuldade da tarefa" />
       </div>
